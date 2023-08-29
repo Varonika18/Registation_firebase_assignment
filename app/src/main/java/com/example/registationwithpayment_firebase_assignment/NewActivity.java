8d4584b4f0d699848161b29b9c6134b2e4ef348e
Varonika <var2020p@gmail.com>
2023-08-29 21:37:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/registationwithpayment_firebase_assignment/NewActivity.java
@@ -0,0 +1,15 @@
+package com.example.registationwithpayment_firebase_assignment;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_new);
+    }
+
+}
